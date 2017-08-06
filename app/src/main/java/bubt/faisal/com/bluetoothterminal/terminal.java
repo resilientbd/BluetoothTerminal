17091b84d922dc431eb053ab0f318a6a5b1b3a27
Shaikh Faisal Hossain <faisalsaptarshi@gmail.com>
2017-08-06 21:44:04 +0600

    My Simpe Bluetooth terminal
    It can be use as the library of any bluetooth base android application.

---
+++ app/src/main/java/bubt/faisal/com/bluetoothterminal/terminal.java
@@ -0,0 +1,13 @@
+package bubt.faisal.com.bluetoothterminal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class terminal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminal);
+    }
+}
